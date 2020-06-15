65cdb09e08162052a4a4ea76d65d7e02f15d7c60
Mr.K <Mr.K@DESKTOP-4JRTA25>
2020-06-15 22:59:41 +0900

    hard coding days

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -8,6 +8,33 @@ public class Calendar {
 	public int getMaxdayOfMonth(int month) {
 		return MAX_DAYS[month-1];
 	}
+	
+	public static void printcal(int selectmonth) {
+		if(MAX_DAYS[selectmonth-1] == 31) {
+			System.out.println("일   월   화   수   목   금   토");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");
+			System.out.println("29 30 31");
+		}
+		else if(MAX_DAYS[selectmonth-1] == 30) {
+			System.out.println("일   월   화   수   목   금   토");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");
+			System.out.println("29 30");
+
+		}
+		else {
+			System.out.println("일   월   화   수   목   금   토");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");						
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -26,7 +53,7 @@ public class Calendar {
 			else if (input > 12) {
 				continue;
 			}
-			System.out.println(cal.getMaxdayOfMonth(input));
+			cal.printcal(input);
 			
 		}
 		
