92c7cd583dfd77980b89af9e6202de07d6b01130
Mr.K <Mr.K@DESKTOP-4JRTA25>
2020-06-15 17:31:20 +0900

    add repeat function

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,24 +3,29 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public int getMaxdayOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("달을 입력하세요:");
-		int i = scanner.nextInt();
+		Calendar cal = new Calendar();
+		System.out.println("반복횟수를 입력하세요:");
+		int times = scanner.nextInt();
+		int[] month = new int[times];
+		System.out.println("월을 입력하세요:");
+		for(int i = 1; i<=times; i++) {
+			month[i-1] = scanner.nextInt();
+		}
+		for(int i = 1; i<=times; i++) {
+			System.out.println(cal.getMaxdayOfMonth(month[i-1]));
+		}
 		
 		
-		if(i==1 || i ==3 || i==5 || i==7 || i==8 ||i==10 ||i==12) {
-			System.out.printf("%d월은 31일까지 있습니다.\n", i);
-		}
-		else if(i==2) {
-			System.out.printf("%d월은 28일까지 있습니다.\n", i);
-		}
-		else
-			System.out.printf("%d월은 30일까지 있습니다.\n", i);
-
 		
 		scanner.close();
 	}
